39088a72617fd74d4c6df95f510a8c2309eb1927
清水 <2756848729@qq.com>
2023-02-27 11:38:22 +0800

    feat: useReducer进一步管理复杂state

--- src/utils/useUndo.ts
+++ src/utils/useUndo.ts
@@ -1,50 +1,46 @@
-import { useCallback, useState } from "react";
+import { useCallback, useReducer } from "react";
 
-export const useUndo = <T>(initialPresent: T) => {
-  const [state, setState] = useState<{
-    past: T[],
-    present: T,
-    future: T[]
-  }>({
-    past: [],
-    present: initialPresent,
-    future: []
-  });
+const SET = 'SET';
+const RESET = 'RESET';
+const UNDO = 'UNDO';
+const REDO = 'REDO';
 
-  /** 设置新的值 */
-  const set = useCallback((newPresent: T) => {
-    setState((currentState) => {
-      const {past, present, future} = currentState;
-      if (newPresent === present) return currentState;
+type State<T> = {
+  past: T[],
+  present: T,
+  future: T[]
+};
 
+type Action<T> = {
+  newPresent?: T,
+  type: typeof SET | typeof RESET | typeof UNDO | typeof REDO;
+}
+
+/** reducer */
+const undoRuducer = <T>(state: State<T>, action: Action<T>) => {
+  // state是现在的值
+  const {past, present, future} = state;
+  const {newPresent, type} = action;
+
+  switch(type) {
+    case SET: {
+      if (newPresent === present) return state;
+      // 返回下一个state值
       return {
         past: [...past, present],
         present: newPresent,
         future: []
-      }
-    })
-  }, []);
-
-  const canUndo = state.past.length !== 0;
-  const canRedo = state.future.length !== 0;
-
-  /** 重置 */
-  const reset = useCallback((newPresent: T) => {
-    setState(() => {
+      };
+    }
+    case RESET: {
       return {
         past: [],
         present: newPresent,
         future: []
-      }
-    })
-  }, []);
-
-  /** 倒回 */
-  const undo = useCallback(() => {
-    setState((currentState) => {
-      const {past, present, future} = currentState;
-
-      if (past.length === 0) return currentState;
+      };
+    }
+    case UNDO: {
+      if (past.length === 0) return state;
 
       const newPresent = past[past.length - 1];
       const newPast = past.slice(0, past.length - 1);
@@ -53,15 +49,9 @@ export const useUndo = <T>(initialPresent: T) => {
         present: newPresent,
         future: [present, ...future]
       }
-    })
-  }, []);
-
-  /** 往前 */
-  const redo = useCallback(() => {
-    setState((currentState) => {
-      const {past, present, future} = currentState;
-
-      if (future.length === 0) return currentState;
+    }
+    case REDO: {
+      if (future.length === 0) return state;
 
       const newPresent = future[0];
       const newFuture = future.slice(1);
@@ -70,8 +60,31 @@ export const useUndo = <T>(initialPresent: T) => {
         present: newPresent,
         future: newFuture
       }
-    })
-  }, []);
+    }
+  }
+}
+
+export const useUndo = <T>(initialPresent: T) => {
+  const [state, dispatch] = useReducer(undoRuducer, {
+    past: [],
+    present: initialPresent,
+    future: []
+  } as State<T>);
+
+  const canUndo = state.past.length !== 0;
+  const canRedo = state.future.length !== 0;
+
+  /** 设置新的值 */
+  const set = useCallback((newPresent: T) => dispatch({newPresent, type: SET}), []);
+
+  /** 重置 */
+  const reset = useCallback((newPresent: T) => dispatch({newPresent, type: RESET}), []);
+
+  /** 倒回 */
+  const undo = useCallback(() => dispatch({type: UNDO}), []);
+
+  /** 往前 */
+  const redo = useCallback(() => dispatch({type: REDO}), []);
 
   return [
     state,
